028b5af9823e48012dc2fdb42377be4989fd548f
ravishverma <ravishvermawork@gmail.com>
2021-11-05 00:05:49 +0100

    model architecture ready

---
+++ NNmodel.py
@@ -0,0 +1,62 @@
+import torch.nn as nn
+import torch.optim as optim
+
+# Input: Grayscale images: 64 x 64 x 1
+
+# Reduce image size to 1/4 x 1/4
+class encodeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(2,2)
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.relu(x)
+        return x
+
+# Expand image size to 4 x 4
+class decodeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.convTrans1 = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=3, stride=2)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.convTrans2 = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+        x = self.convTrans1(x)
+        x = self.conv1(x)
+        x = self.convTrans2(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        return x
+
+# Autoencoder
+class autoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.down1 = encodeBlock(1,3) # 64x64x1 > 16x16x3
+        self.down2 = encodeBlock(3,5) # 16x16x3 > 4x4x5
+        self.down3 = encodeBlock(5,2) # 4x4x5 > 1x1x2
+        self.up1 = decodeBlock(2,5) 
+        self.up2 = decodeBlock(5,3) 
+        self.up3 = decodeBlock(3,1) 
+
+    def forward(x):
+        x = self.down1(x)
+        x = self.down2(x)
+        x = self.down3(x)
+        ls = x.flatten()
+
+        x = self.up1(x)
+        x = self.up2(x)
+        x = self.up3(x)
+        return ls, x     
