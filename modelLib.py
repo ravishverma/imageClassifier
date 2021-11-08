3c53395965f09d5d1f19afe93b3d15a84aa0f7df
ravishverma <ravishvermawork@gmail.com>
2021-11-08 22:51:51 +0000

    model with more non-linearity but linear LS

--- modelLib.py
+++ modelLib.py
@@ -5,13 +5,14 @@ import torch.optim as optim
 
 # Reduce image size to 1/4 x 1/4
 class encodeBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, last=False):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.pool1 = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.pool2 = nn.MaxPool2d(2,2)
         self.relu = nn.ReLU()
+        self.last = last
 
     def forward(self,x):
         x = self.conv1(x)
@@ -19,18 +20,19 @@ class encodeBlock(nn.Module):
         x = self.relu(x)
         x = self.conv2(x)
         x = self.pool2(x)
-        x = self.relu(x)
+        x = self.relu(x) if not self.last else x
         return x
 
 # Expand image size to 4 x 4
 class decodeBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, last=False):
         super().__init__()
         self.convTrans1 = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.convTrans2 = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
+        self.last = last
 
     def forward(self,x):
         x = self.convTrans1(x)
@@ -38,7 +40,7 @@ class decodeBlock(nn.Module):
         x = self.relu(x)
         x = self.convTrans2(x)
         x = self.conv2(x)
-        x = self.relu(x)
+        x = self.relu(x) if not self.last else x
         return x
 
 # Autoencoder
@@ -47,10 +49,10 @@ class autoencoder(nn.Module):
         super().__init__()
         self.down1 = encodeBlock(1,3) # 1x64x64 > 3x16x16
         self.down2 = encodeBlock(3,5) # 3x16x16 > 5x4x4
-        self.down3 = encodeBlock(5,5) # 5x4x4 > 2x1x1
+        self.down3 = encodeBlock(5,5, last=True) # 5x4x4 > 2x1x1
         self.up1 = decodeBlock(5,5) 
         self.up2 = decodeBlock(5,3) 
-        self.up3 = decodeBlock(3,1) 
+        self.up3 = decodeBlock(3,1, last=True) 
 
     def forward(self,x):
         x = self.down1(x)
