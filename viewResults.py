de49fe459bdbb8f3244252eac4b4fd66d0ff4480
ravishverma <ravishvermawork@gmail.com>
2021-11-16 16:47:04 +0000

    Method to view results. first results with two groups (lowres)

---
+++ viewResults.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from environment import *
+import os
+from matplotlib import pyplot as plt
+import sys
+import torch
+import torchvision
+from dataLoader import *
+from modelLib import *
+
+chkptFile = sys.argv[1]
+
+model = autoencoder().to(DEVICE)
+model, _, _, _, _ = loadChkPt(chkptFile, model)
+dataset = myDataset(path=DATADIR)
+
+df = pd.read_csv(os.path.join(RESULTSDIR,"labeledData.csv"), index_col=0)
+
+uniqueLabels = sorted(df["labels"].unique())
+
+fig, axes = plt.subplots(len(uniqueLabels),1)
+
+for i, uL in enumerate(uniqueLabels):
+    sample = df[df["labels"]==uL].sample(n=4)["fileName"].tolist()
+    grid = []
+    for s in sample:
+        img = dataset.loadFromFile(os.path.join(DATADIR,s))
+        grid.append(img.reshape(1,64,64))
+    gridImg = torchvision.utils.make_grid(grid,nrow=4)
+    gridImg = gridImg.permute(1,2,0)
+    axes[i].imshow(gridImg)
+    axes[i].set_ylabel("Label "+str(uL))
+
+plt.savefig(os.path.join(RESULTSDIR,"results.png"))
+plt.close()
